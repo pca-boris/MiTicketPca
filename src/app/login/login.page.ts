b08e66d575fa71e9d6f7da41af240c0d58f3ab91
Boris Bonett Jaramillo <borisbonettjaramillo@Boriss-MacBook-Pro.local>
2024-02-02 20:35:48 -0500

    se agrego validaciones de email

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -9,8 +11,19 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 export class LoginPage implements OnInit {
 
   loginForm: FormGroup;
+  validation_messages = {
+    email:[
+      { type: 'required', message: 'Email is required.' },
+      { type: 'pattern', message: 'Please enter a valid email.' }
+    ]
+  }
+  loginMessage: any;
 
-  constructor(private formBuilder:FormBuilder) {  
+  constructor(
+      private formBuilder:FormBuilder,
+      private authService:AuthService,
+      private navCtrl:NavController,
+    ) {  
 
     this.loginForm = this.formBuilder.group({
       email: new FormControl("", Validators.compose([
@@ -31,6 +44,13 @@ export class LoginPage implements OnInit {
 
   public login(login_data:any): void {
     console.log(login_data);
+    this.authService.loginUser(login_data).then((res:any) => {
+      console.log(res);
+      this.loginMessage = res;
+      this.navCtrl.navigateForward('/home');
+    }).catch((err:any) => {
+      this.loginMessage = err;
+    })
   }
 
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+  loginUser(credential: any) {
+    return new Promise((resolve, reject) => {
+      if(credential.email == 'bonett.boris@gmail.com' && credential.password == '123456') {
+        resolve('auth success');
+      } else {
+        reject('auth failed');
+      }
+    })
+  }
+}
